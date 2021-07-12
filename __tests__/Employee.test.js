1434627a3fde80f168e0b855a0fc020b279909d0
Junny <tajo0215@gmail.com>
2021-07-11 22:25:51 -0700

    Employee.test.js created tests and passed all

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee.js');
+const employee = new Employee('Jun', 'tajo0215', 'tajo0215@gmail.com');
+
+describe('Employee', () => {
+  describe('constructor', () => {
+    it('should create an Employee object', () => {
+      expect(employee.name).toEqual('Jun');
+      expect(employee.id).toEqual('tajo0215');
+      expect(employee.email).toEqual('tajo0215@gmail.com');
+    });
+  });
+
+  describe('getName', () => {
+    it('should get employee name', () => {
+      expect(employee.getName()).toEqual('Jun');
+    });
+  });
+
+  describe('getId', () => {
+    it('should get employee name', () => {
+      expect(employee.getId()).toEqual('tajo0215');
+    });
+  });
+
+  describe('getEmail', () => {
+    it('should get employee name', () => {
+      expect(employee.getEmail()).toEqual('tajo0215@gmail.com');
+    });
+  });
+});
