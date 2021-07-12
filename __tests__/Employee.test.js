af44e373e8fb51e26509ed7e23200fa680a21b9e
Junny <tajo0215@gmail.com>
2021-07-11 22:32:52 -0700

    Tests created for Manager Class and passed all tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,9 @@
 const Employee = require('../lib/Employee.js');
 const employee = new Employee('Jun', 'tajo0215', 'tajo0215@gmail.com');
 
+const Manager = require('../lib/Manager.js');
+const manager = new Manager('Jun', 'tajo0215', 'tajo0215@gmail.com', 1234);
+
 describe('Employee', () => {
   describe('constructor', () => {
     it('should create an Employee object', () => {
@@ -17,14 +20,44 @@ describe('Employee', () => {
   });
 
   describe('getId', () => {
-    it('should get employee name', () => {
+    it('should get employee id', () => {
       expect(employee.getId()).toEqual('tajo0215');
     });
   });
 
   describe('getEmail', () => {
-    it('should get employee name', () => {
+    it('should get employee email', () => {
       expect(employee.getEmail()).toEqual('tajo0215@gmail.com');
+    });
+  });
+
+  describe('getRole', () => {
+    it('should get employee role', () => {
+      expect(employee.getRole()).toEqual('Employee');
+    });
+  });
+});
+
+
+describe('Manager', () => {
+  describe('constructor', () => {
+    it('should create an Manager object', () => {
+      expect(manager.name).toEqual('Jun');
+      expect(manager.id).toEqual('tajo0215');
+      expect(manager.email).toEqual('tajo0215@gmail.com');
+      expect(manager.officeNumber).toEqual(1234);
+    });
+  });
+
+  describe('getOfficeNumber', () => {
+    it('should get officeNumber property', () => {
+      expect(manager.getOfficeNumber()).toEqual(1234);
+    });
+  });
+
+  describe('getRole', () => {
+    it('should get Manager role', () => {
+      expect(manager.getRole()).toEqual('Manager');
     });
   });
 });
