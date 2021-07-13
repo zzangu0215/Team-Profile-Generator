00a0ee0555c3f773c606c0d688d3e9cc7c393bfd
Junny <tajo0215@gmail.com>
2021-07-13 09:48:06 -0700

    bootstrap styling completed

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -24,7 +24,7 @@ const htmlBlock = function (teamMembers) {
 
       <main>
         <div class="container">
-          <div class="row justify-content-center" id="entire-team">
+          <div class="d-flex flex-wrap justify-content-between" id="entire-team">
             ${teamMembers}
           </div>
         </div>
@@ -39,19 +39,19 @@ const htmlBlock = function (teamMembers) {
 
 function appendManager(manager) {
   const managerBlock = `
-  <div class="card border-secondary mb-3 mt-4">
-    <div class="card-header" id="manager">
-      <h3>${manager.name}</h3>
-      <h4><i class="bi bi-person-badge-fill"></i><strong> Manager</strong></h4>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" id="id"><strong>ID: </strong>${manager.id}</li>
-        <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${manager.email}">${manager.email}</a></li>
-        <li class="list-group-item" id="office-number"><strong>Office Number: </strong>${manager.officeNumber}</li>
-      </ul>
-    </div>
-   </div>
+            <div class="card border-secondary mb-3 mt-4 mx-auto">
+              <div class="card-header" id="manager">
+                <h3>${manager.name}</h3>
+                <h4><i class="bi bi-person-badge-fill"></i><strong> Manager</strong></h4>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" id="id"><strong>ID: </strong>${manager.id}</li>
+                  <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${manager.email}">${manager.email}</a></li>
+                  <li class="list-group-item" id="office-number"><strong>Office Number: </strong>${manager.officeNumber}</li>
+                </ul>
+              </div>
+            </div>
   `;
 
   return managerBlock;
@@ -59,19 +59,19 @@ function appendManager(manager) {
 
 function appendEngineer(engineer) {
   const engineerBlock = `
-  <div class="card border-secondary mb-3 mt-4">
-    <div class="card-header" id="engineer">
-      <h3>${engineer.name}</h3>
-      <h4><i class="bi bi-laptop"></i><strong> Engineer</strong></h4>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" id="id"><strong>ID: </strong>${engineer.id}</li>
-        <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${engineer.email}">${engineer.email}</a></li>
-        <li class="list-group-item" id="github"><strong>Github: </strong><a href="https://github.com/${engineer.github}/">${engineer.github}</a></li>
-      </ul>
-    </div>
-   </div>
+            <div class="card border-secondary mb-3 mt-4 mx-auto">
+              <div class="card-header" id="engineer">
+                <h3>${engineer.name}</h3>
+                <h4><i class="bi bi-laptop"></i><strong> Engineer</strong></h4>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" id="id"><strong>ID: </strong>${engineer.id}</li>
+                  <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                  <li class="list-group-item" id="github"><strong>Github: </strong><a href="https://github.com/${engineer.github}/">${engineer.github}</a></li>
+                </ul>
+              </div>
+            </div>
   `;
 
   return engineerBlock;
@@ -79,19 +79,19 @@ function appendEngineer(engineer) {
 
 function appendIntern(intern) {
   const internBlock = `
-  <div class="card border-secondary mb-3 mt-4">
-    <div class="card-header" id="intern">
-      <h3>${intern.name}</h3>
-      <h4><i class="bi bi-laptop"></i><strong> Intern</strong></h4>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" id="id"><strong>ID: </strong>${intern.id}</li>
-        <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${intern.email}">${intern.email}</a></li>
-        <li class="list-group-item" id="school"><strong>School: </strong>${intern.school}</li>
-      </ul>
-    </div>
-   </div>
+            <div class="card border-secondary mb-3 mt-4 mx-auto">
+              <div class="card-header" id="intern">
+                <h3>${intern.name}</h3>
+                <h4><i class="bi bi-laptop"></i><strong> Intern</strong></h4>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" id="id"><strong>ID: </strong>${intern.id}</li>
+                  <li class="list-group-item" id="email"><strong>Email: </strong><a href="mailto:${intern.email}">${intern.email}</a></li>
+                  <li class="list-group-item" id="school"><strong>School: </strong>${intern.school}</li>
+                </ul>
+              </div>
+            </div>
   `;
 
   return internBlock;
